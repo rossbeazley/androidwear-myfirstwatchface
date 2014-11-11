5eb823261ce47afc7479ff7300c6240fcf62e7db
rossbeazley <ross@rossbeazley.co.uk>
2014-11-11 15:50:44 +0000

    Came up with some tests for Seconds

---
+++ core/src/test/java/uk/co/rossbeazley/wear/seconds/SecondsChange.java
@@ -0,0 +1,24 @@
+package uk.co.rossbeazley.wear.seconds;
+
+import org.junit.Test;
+
+/**
+ * Created by beazlr02 on 11/11/2014.
+ */
+public class SecondsChange {
+
+    @Test
+    public void theOneWhereTheSecondsUpdate() {
+
+    }
+
+    @Test
+    public void theOneWhereTheSecondsDontUpdate() {
+
+    }
+
+    @Test
+    public void theOneWhereTheSecondsRoleOver() {
+
+    }
+}
