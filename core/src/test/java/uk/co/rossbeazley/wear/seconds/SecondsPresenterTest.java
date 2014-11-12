4b69963e5a921e0a15bc0363851e83df8b0fa448
rossbeazley <ross@rossbeazley.co.uk>
2014-11-12 11:41:21 +0000

    Thought about the first test for the seconds presenter

---
+++ core/src/test/java/uk/co/rossbeazley/wear/seconds/SecondsPresenterTest.java
@@ -0,0 +1,15 @@
+package uk.co.rossbeazley.wear.seconds;
+
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class SecondsPresenterTest {
+
+    @Test
+    @Ignore
+    public void theOneWhereWeUpdateTheScreen() {
+
+    }
+
+}
