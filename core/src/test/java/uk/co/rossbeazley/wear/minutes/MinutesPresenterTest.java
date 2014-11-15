28e9df9a4ea9048dd5777478789a73a7d666f9a7
rossbeazley <ross@rossbeazley.co.uk>
2014-11-15 21:15:50 +0000

    Test for minutes presenter sorted, time to get green

---
+++ core/src/test/java/uk/co/rossbeazley/wear/minutes/MinutesPresenterTest.java
@@ -0,0 +1,43 @@
+package uk.co.rossbeazley.wear.minutes;
+
+import org.junit.Test;
+
+import uk.co.rossbeazley.wear.Sexagesimal;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MinutesPresenterTest {
+
+    private String timeComponentString;
+    private CanBeObservedForChangesToMinutes.CanReceiveMinutesUpdates observer;
+
+    @Test
+    public void theOneWhereWeUpdateTheScreen() {
+        CanBeObservedForChangesToMinutes canBeObservedForChangesToMinutes = new CanBeObservedForChangesToMinutes(){
+            @Override public void observe(CanReceiveMinutesUpdates canReceiveSecondsUpdates) {
+                observer = canReceiveSecondsUpdates;
+            }
+        };
+
+        MinutesPresenter.MinutesView view = new MinutesPresenter.MinutesView(){
+            @Override public void showMinutesString(String seconds) {
+                timeComponentString = seconds;
+            }
+        };
+
+        MinutesPresenter presenter = new MinutesPresenter(canBeObservedForChangesToMinutes, view);
+        observer.minutesUpdate(Sexagesimal.fromBase10(10));
+        assertThat(timeComponentString, is("10"));
+    }
+
+    private static class MinutesPresenter {
+
+        public interface MinutesView {
+            void showMinutesString(String seconds);
+        }
+
+        public MinutesPresenter(CanBeObservedForChangesToMinutes canBeObservedForChangesToMinutes, MinutesView view) {
+        }
+    }
+}
