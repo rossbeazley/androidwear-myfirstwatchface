1f474c3778477f1354ab423ccb66b2dddd56d88b
rossbeazley <ross@rossbeazley.co.uk>
2014-11-16 18:01:41 +0000

    hour base 24 seams to make more sense, especially as its about to attract some am/pm stuff

--- core/src/test/java/uk/co/rossbeazley/wear/hours/HoursPresenterTest.java
+++ core/src/test/java/uk/co/rossbeazley/wear/hours/HoursPresenterTest.java
@@ -14,7 +14,7 @@ public class HoursPresenterTest {
         Hours hours = new Hours();
         HoursView hoursView = new HoursView();
         new HoursPresenter(hours, hoursView);
-        hours.observer.hoursUpdate(Base24.fromBase10(8));
+        hours.observer.hoursUpdate(HourBase24.fromBase10(8));
         assertThat(hoursView.timeComponentString, is("08"));
     }
 
@@ -23,19 +23,19 @@ public class HoursPresenterTest {
         Hours hours = new Hours();
         HoursView hoursView = new HoursView();
         new HoursPresenter(hours, hoursView);
-        hours.observer.hoursUpdate(Base24.fromBase10(14));
+        hours.observer.hoursUpdate(HourBase24.fromBase10(14));
         assertThat(hoursView.timeComponentString, is("02"));
     }
 
-    private static class Base24 {
+    private static class HourBase24 {
         private final int value;
 
-        public Base24(int base10) {
+        public HourBase24(int base10) {
             value = base10 % 24;
         }
 
-        public static Base24 fromBase10(int base10) {
-            return new Base24(base10);
+        public static HourBase24 fromBase10(int base10) {
+            return new HourBase24(base10);
         }
 
         public String toBase10TwelveHour() {
@@ -48,8 +48,8 @@ public class HoursPresenterTest {
         public HoursPresenter(CanBeObservedForChangesToHours canBeObservedForChangesToHours, final HoursView hoursView) {
             canBeObservedForChangesToHours.observe(new CanBeObservedForChangesToHours.CanReceiveHoursUpdates() {
                 @Override
-                public void hoursUpdate(Base24 base24) {
-                    hoursView.showHoursString(base24.toBase10TwelveHour());
+                public void hoursUpdate(HourBase24 hourBase24) {
+                    hoursView.showHoursString(hourBase24.toBase10TwelveHour());
                 }
             });
         }
@@ -63,7 +63,7 @@ public class HoursPresenterTest {
         public void observe(CanReceiveHoursUpdates canReceiveHoursUpdates);
 
         interface CanReceiveHoursUpdates {
-            void hoursUpdate(Base24 base24);
+            void hoursUpdate(HourBase24 hourBase24);
         }
     }
 
