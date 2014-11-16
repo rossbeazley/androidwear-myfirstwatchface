04fdddc73576e4907882425c09a78745049d845a
rossbeazley <ross@rossbeazley.co.uk>
2014-11-16 13:33:10 +0000

    thought of two tests for the hours presenter

---
+++ core/src/test/java/uk/co/rossbeazley/wear/hours/HoursPresenterTest.java
@@ -0,0 +1,16 @@
+package uk.co.rossbeazley.wear.hours;
+
+import org.junit.Test;
+
+public class HoursPresenterTest {
+
+    @Test
+    public void theOneWhereItsMorningAndTheHourUpdates() {
+
+    }
+
+    @Test
+    public void theOneWhereItsEveningAndTheHourUpdates() {
+
+    }
+}
