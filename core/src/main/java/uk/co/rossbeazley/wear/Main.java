824b6f7aeb8578face304e58ec1d2fac24c20e27
rossbeazley <ross@rossbeazley.co.uk>
2014-11-16 08:49:05 +0000

    refactored main so there is a separate core and shell

--- core/src/main/java/uk/co/rossbeazley/wear/Main.java
+++ core/src/main/java/uk/co/rossbeazley/wear/Main.java
@@ -9,7 +9,7 @@ import uk.co.rossbeazley.wear.ticktock.TickTock;
 public class Main {
 
     private static Main instance;
-    public CanBeObservedForChangesToMinutes canBeObservedForChangesToMinutes;
+    public final Core core;
 
     public static Main instance() { return instance; }
 
@@ -17,16 +17,28 @@ public class Main {
         instance = new Main();
     }
 
-    public final CanBeObservedForChangesToSeconds canBeObservedForChangesToSeconds;
-
     public Main() {
-        Seconds seconds = new Seconds();
-        canBeObservedForChangesToSeconds = seconds;
 
-        MinutesFromTick minutes = new MinutesFromTick();
-        canBeObservedForChangesToMinutes = minutes;
+        this.core = new Core();
 
-        TickTock tickTock = TickTock.createTickTock(seconds, minutes);
+        TickTock tickTock = TickTock.createTickTock(core.seconds, core.minutes);
+    }
+
+    public static class Core {
+
+        public final CanBeObservedForChangesToMinutes canBeObservedForChangesToMinutes;
+        public final CanBeObservedForChangesToSeconds canBeObservedForChangesToSeconds;
+
+        private Seconds seconds;
+        private MinutesFromTick minutes;
+
+        Core() {
+            seconds = new Seconds();
+            minutes = new MinutesFromTick();
+            canBeObservedForChangesToSeconds = seconds;
+            canBeObservedForChangesToMinutes = minutes;
+
+        }
     }
 
 }
