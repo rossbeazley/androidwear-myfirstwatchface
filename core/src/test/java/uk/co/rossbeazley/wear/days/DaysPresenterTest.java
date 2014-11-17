e8ddb272d28c58ab1202e26291fb08f7a464b844
rossbeazley <ross@rossbeazley.co.uk>
2014-11-17 17:53:21 +0000

    thought of the first test, off the back of this i might actually have to test the day VO directly

---
+++ core/src/test/java/uk/co/rossbeazley/wear/days/DaysPresenterTest.java
@@ -0,0 +1,11 @@
+package uk.co.rossbeazley.wear.days;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class DaysPresenterTest {
+    @Test @Ignore("task list test")
+    public void theOneWhereTheDayUpdates() {
+
+    }
+}
