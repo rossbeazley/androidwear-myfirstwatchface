318994777640e393aaf5b298c23939a445d7d02d
rossbeazley <ross@rossbeazley.co.uk>
2014-11-18 16:41:28 +0000

    days presenter test written, ive ended up writing quite a lot of stuff here

--- core/src/test/java/uk/co/rossbeazley/wear/days/DaysPresenterTest.java
+++ core/src/test/java/uk/co/rossbeazley/wear/days/DaysPresenterTest.java
@@ -1,11 +1,65 @@
 package uk.co.rossbeazley.wear.days;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 public class DaysPresenterTest {
-    @Test @Ignore("task list test")
+    private String dateString;
+    private CanBeObservedForChangesToDays.CanReceiveDaysUpdates daysChange;
+
+    @Test
     public void theOneWhereTheDayUpdates() {
+
+        DaysView daysView = new DaysView() {
+            @Override public void showDaysString(String newDays) {
+                dateString = newDays;
+            }
+        };
+
+        CanBeObservedForChangesToDays days = new CanBeObservedForChangesToDays() {
+            @Override
+            public void observe(CanReceiveDaysUpdates canReceiveDaysUpdates) {
+                DaysPresenterTest.this.daysChange = canReceiveDaysUpdates;
+            }
+        };
+
+        daysChange.daysUpdate(new Day(1));
+
+        new DaysPresenter(days, daysView);
+
+        assertThat(dateString, is("1st"));
+    }
+
+    private class DaysPresenter {
+        public DaysPresenter(CanBeObservedForChangesToDays days, DaysView daysView) {
+
+        }
+    }
+
+    private interface DaysView {
+        void showDaysString(String newDays);
+    }
+
+    private interface CanBeObservedForChangesToDays {
+        void observe(CanReceiveDaysUpdates canReceiveSecondsUpdates);
+
+        interface CanReceiveDaysUpdates {
+            void daysUpdate(Day to);
+        }
+    }
+
+    private class Day {
+        private final int value;
+
+        public Day(int i) {
+            this.value = i;
+        }
+
+        public String toOrdinalString() {
+            return value+"st";
+        }
 
     }
 }
