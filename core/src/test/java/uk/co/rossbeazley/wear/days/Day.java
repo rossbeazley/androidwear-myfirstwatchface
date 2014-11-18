b9415f995f9e0f94db41277fa632203ae70e6478
rossbeazley <ross@rossbeazley.co.uk>
2014-11-18 22:45:47 +0000

    extracted an ordinal object for dates

--- core/src/test/java/uk/co/rossbeazley/wear/days/Day.java
+++ core/src/test/java/uk/co/rossbeazley/wear/days/Day.java
@@ -2,6 +2,8 @@ package uk.co.rossbeazley.wear.days;
 
 import java.util.HashMap;
 
+import static java.lang.String.*;
+
 /**
 * Created by beazlr02 on 18/11/2014.
 */
@@ -17,23 +19,42 @@ class Day {
     }
 
     public String toOrdinalString() {
-        return value + ordinalForValue(value);
+        return format("%d%s", value, Ordinal.forValue(value));
     }
 
-    private String ordinalForValue(int value) {
-        HashMap<Integer, String> lookupTable;
-        lookupTable = new HashMap<Integer, String>() {{
-            put(1,"st");
-            put(2,"nd");
-            put(3,"rd");
-            put(21,"st");
-            put(22,"nd");
-            put(23,"rd");
-            put(31,"st");
-        }};
 
-        String DEFAULT = "th";
-        return lookupTable.containsKey(value)? lookupTable.get(value) : DEFAULT;
+
+    private static class Ordinal {
+
+        final String ST = "st", ND = "nd", RD = "rd", TH = "th";
+        final String DEFAULT = TH;
+
+        private final int value;
+        private HashMap<Integer, String> lookupTable;
+
+        private Ordinal(int value) {
+            this.value = value;
+            lookupTable = new HashMap<Integer, String>() {{
+                put(1, ST);
+                put(2, ND);
+                put(3, RD);
+                put(21, ST);
+                put(22, ND);
+                put(23, RD);
+                put(31, ST);
+            }};
+        }
+
+        @Override
+        public String toString() {
+            return lookupTable.containsKey(value)? lookupTable.get(value) : DEFAULT;
+        }
+
+        private static Ordinal forValue(int value) {
+            return new Ordinal(value);
+        }
     }
+
+
 
 }
