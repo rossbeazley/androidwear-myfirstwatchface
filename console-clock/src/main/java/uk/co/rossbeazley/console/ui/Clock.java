052b3bb857c6f9cdaf38076e845224ce260029bb
rossbeazley <ross@rossbeazley.co.uk>
2014-11-26 12:48:46 +0000

    bit of an refactor

--- console-clock/src/main/java/uk/co/rossbeazley/console/ui/Clock.java
+++ console-clock/src/main/java/uk/co/rossbeazley/console/ui/Clock.java
@@ -9,7 +9,7 @@ import uk.co.rossbeazley.wear.seconds.SecondsPresenter;
 
 public class Clock {
 
-    public static void main(String... vargs) {
+    public Clock() {
         Main.init();
         Main main = Main.instance();
         ConsoleView view = new ConsoleView();
@@ -20,46 +20,8 @@ public class Clock {
         new SecondsPresenter(main.core.canBeObservedForChangesToSeconds, view);
     }
 
-    private static class ConsoleView implements DaysPresenter.DaysView, MonthsPresenter.MonthView, HoursPresenter.HoursView, MinutesPresenter.MinutesView, SecondsPresenter.SecondsView {
-
-        private String newDays;
-        private String monthString;
-        private String hours;
-        private String minutes;
-        private String seconds;
-
-        private void redraw() {
-            System.out.println(hours + ":" + minutes + ":" + seconds + " " + newDays + " " + monthString);
-        }
-
-        @Override
-        public void showDaysString(String newDays) {
-            this.newDays = newDays;
-            redraw();
-        }
-
-        @Override
-        public void showMonthString(String monthString) {
-            this.monthString = monthString;
-            redraw();
-        }
-
-        @Override
-        public void showHoursString(String hours) {
-            this.hours = hours;
-            redraw();
-        }
-
-        @Override
-        public void showMinutesString(String minutes) {
-            this.minutes = minutes;
-            redraw();
-        }
-
-        @Override
-        public void showSecondsString(String seconds) {
-            this.seconds = seconds;
-            redraw();
-        }
+    public static void main(String... vargs) {
+        new Clock();
     }
+
 }
