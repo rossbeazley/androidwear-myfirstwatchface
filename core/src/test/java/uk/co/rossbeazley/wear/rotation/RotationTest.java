1bff5b893a80b9bb71b3e67217c78e3b2fcde9ec
rossbeazley <ross@rossbeazley.co.uk>
2014-11-26 22:28:19 +0000

    rotation test list

---
+++ core/src/test/java/uk/co/rossbeazley/wear/rotation/RotationTest.java
@@ -0,0 +1,31 @@
+package uk.co.rossbeazley.wear.rotation;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class RotationTest {
+
+    @Test @Ignore("test list")
+    public void theOneWhereWeRotateRightToEast() {
+
+    }
+
+    @Test @Ignore("test list")
+    public void theOneWhereWeRotateRightToSouth() {
+
+    }
+
+
+    @Test @Ignore("test list")
+    public void theOneWhereWeRotateRightToWest() {
+
+    }
+
+
+    @Test @Ignore("test list")
+    public void theOneWhereWeRotateRightToNorth() {
+
+    }
+
+
+}
