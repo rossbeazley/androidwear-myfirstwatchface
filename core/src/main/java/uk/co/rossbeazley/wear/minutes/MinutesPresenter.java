2f178c3a55f4370d82978788c6296b83f4569219
rossbeazley <ross@rossbeazley.co.uk>
2014-12-12 21:41:56 +0000

    test for minutes unobservation - passing

--- core/src/main/java/uk/co/rossbeazley/wear/minutes/MinutesPresenter.java
+++ core/src/main/java/uk/co/rossbeazley/wear/minutes/MinutesPresenter.java
@@ -5,21 +5,27 @@ import uk.co.rossbeazley.wear.ui.Disposable;
 
 public class MinutesPresenter implements Disposable {
 
-    @Override
-    public void dispose() {
-
-    }
+    private final CanBeObservedForChangesToMinutes.CanReceiveMinutesUpdates updateView;
+    private final CanBeObservedForChangesToMinutes canBeObservedForChangesToMinutes;
 
     public interface MinutesView {
         void showMinutesString(String seconds);
     }
 
     public MinutesPresenter(final CanBeObservedForChangesToMinutes canBeObservedForChangesToMinutes, final MinutesView view) {
-        canBeObservedForChangesToMinutes.observe(new CanBeObservedForChangesToMinutes.CanReceiveMinutesUpdates() {
+        this.canBeObservedForChangesToMinutes = canBeObservedForChangesToMinutes;
+        updateView = new CanBeObservedForChangesToMinutes.CanReceiveMinutesUpdates() {
             @Override
             public void minutesUpdate(Sexagesimal to) {
                 view.showMinutesString(to.base10String());
             }
-        });
+        };
+        canBeObservedForChangesToMinutes.observe(updateView);
     }
+
+    @Override
+    public void dispose() {
+        canBeObservedForChangesToMinutes.unObserve(updateView);
+    }
+
 }
