c23f79357f21629fd75a6700ca113f771836c402
rossbeazley <ross@rossbeazley.co.uk>
2014-12-14 22:35:57 +0000

    months domain observation separation: interface now extends generic one

--- core/src/main/java/uk/co/rossbeazley/wear/months/CanBeObservedForChangesToMonths.java
+++ core/src/main/java/uk/co/rossbeazley/wear/months/CanBeObservedForChangesToMonths.java
@@ -1,10 +1,10 @@
 package uk.co.rossbeazley.wear.months;
 
+import uk.co.rossbeazley.wear.CanBeObserved;
+
 /**
  * Created by beazlr02 on 20/11/2014.
  */
-public interface CanBeObservedForChangesToMonths<t> {
-    void addListener(t canReceiveMonthsUpdates);
-    void removeListener(t canReceiveMonthsUpdates);
+public interface CanBeObservedForChangesToMonths<t> extends CanBeObserved<t>{
 
 }
