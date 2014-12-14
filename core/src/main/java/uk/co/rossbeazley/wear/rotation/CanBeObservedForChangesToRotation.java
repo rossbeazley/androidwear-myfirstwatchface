b4abb1bff1459a97a9220a1bf2a500f5c072c3c8
rossbeazley <ross@rossbeazley.co.uk>
2014-12-14 22:51:53 +0000

    rotation domain observation separation: now extends the generic interface

--- core/src/main/java/uk/co/rossbeazley/wear/rotation/CanBeObservedForChangesToRotation.java
+++ core/src/main/java/uk/co/rossbeazley/wear/rotation/CanBeObservedForChangesToRotation.java
@@ -1,10 +1,10 @@
 package uk.co.rossbeazley.wear.rotation;
 
+import uk.co.rossbeazley.wear.CanBeObserved;
+
 /**
 * Created by beazlr02 on 27/11/2014.
 */
-public interface CanBeObservedForChangesToRotation<T> {
-    void addListener(T canReceiveRotationUpdates);
-    void removeListener(T canReceiveRotationUpdates);
+public interface CanBeObservedForChangesToRotation<T> extends CanBeObserved<T>{
 
 }
