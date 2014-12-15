f5968efb6cc6ea02aceb2eb8945fe69a820b65ac
rossbeazley <ross@rossbeazley.co.uk>
2014-12-15 22:32:29 +0000

    pulling out classes for the ui navigation

---
+++ wear/src/main/java/com/examples/myfirstwatchface/DisplayManagerToWatchFaceAdapter.java
@@ -0,0 +1,41 @@
+package com.examples.myfirstwatchface;
+
+import android.hardware.display.DisplayManager;
+import android.view.Display;
+
+class DisplayManagerToWatchFaceAdapter implements DisplayManager.DisplayListener {
+    private final WatchFace watchFace;
+    private final DisplayManager displayManager;
+
+    public DisplayManagerToWatchFaceAdapter(WatchFace watchFace, DisplayManager displayManager) {
+        this.watchFace = watchFace;
+        this.displayManager = displayManager;
+    }
+
+    @Override
+    public void onDisplayRemoved(int displayId) {
+        this.watchFace.removed();
+    }
+
+    @Override
+    public void onDisplayAdded(int displayId) {
+        //  In testing, this was never called, so the callback for this was removed.
+    }
+
+    @Override
+    public void onDisplayChanged(int displayId) {
+        switch(this.displayManager.getDisplay(displayId).getState()){
+            case Display.STATE_DOZING:
+                this.watchFace.screenDim();
+                break;
+            case Display.STATE_OFF:
+                this.watchFace.screenOff();
+                break;
+            default:
+                //  Not really sure what to so about Display.STATE_UNKNOWN, so
+                //  we'll treat it as if the screen is normal.
+                this.watchFace.screenAwake();
+                break;
+        }
+    }
+}
