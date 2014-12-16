95ace7150acede874667c2dd5077ab55acae6085
rossbeazley <ross@rossbeazley.co.uk>
2014-12-16 12:51:35 +0000

    hours disposal working

--- core/src/main/java/uk/co/rossbeazley/wear/hours/HoursPresenter.java
+++ core/src/main/java/uk/co/rossbeazley/wear/hours/HoursPresenter.java
@@ -1,15 +1,26 @@
 package uk.co.rossbeazley.wear.hours;
 
 import uk.co.rossbeazley.wear.CanBeObserved;
+import uk.co.rossbeazley.wear.ui.Disposable;
 
-public class HoursPresenter {
+public class HoursPresenter implements Disposable {
+    private final CanBeObserved<CanReceiveHoursUpdates> canBeObservedForChangesToHours;
+    public final CanReceiveHoursUpdates updateView;
+
     public HoursPresenter(CanBeObserved<CanReceiveHoursUpdates> canBeObservedForChangesToHours, final HoursView hoursView) {
-        canBeObservedForChangesToHours.addListener(new CanReceiveHoursUpdates() {
+        this.canBeObservedForChangesToHours = canBeObservedForChangesToHours;
+        updateView = new CanReceiveHoursUpdates() {
             @Override
             public void hoursUpdate(HourBase24 hourBase24) {
                 hoursView.showHoursString(hourBase24.toBase10TwelveHour());
             }
-        });
+        };
+        canBeObservedForChangesToHours.addListener(updateView);
+    }
+
+    @Override
+    public void dispose() {
+        canBeObservedForChangesToHours.removeListener(updateView);
     }
 
     public static interface HoursView {
