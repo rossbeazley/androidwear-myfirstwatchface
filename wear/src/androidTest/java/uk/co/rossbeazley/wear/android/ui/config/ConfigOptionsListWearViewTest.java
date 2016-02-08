4f67e54bb049e7392fefbf356a0777c96caadd67
rossbeazley <ross@rossbeazley.co.uk>
2016-02-08 20:16:57 +0000

    instrumentation test working with a sleep, going to migrate to espresso

--- wear/src/androidTest/java/uk/co/rossbeazley/wear/android/ui/config/ConfigOptionsListWearViewTest.java
+++ wear/src/androidTest/java/uk/co/rossbeazley/wear/android/ui/config/ConfigOptionsListWearViewTest.java
@@ -1,14 +1,25 @@
 package uk.co.rossbeazley.wear.android.ui.config;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
+import android.os.SystemClock;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.annotation.UiThreadTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.rule.UiThreadTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.AndroidTestCase;
+import android.support.wearable.view.WearableListView;
+import android.test.InstrumentationTestCase;
 import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.accessibility.AccessibilityWindowInfo;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
@@ -22,61 +33,123 @@ import org.junit.runners.model.Statement;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import uk.co.rossbeazley.wear.R;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 
-@RunWith(AndroidJUnit4.class)
+//@RunWith(AndroidJUnit4.class)
 public class ConfigOptionsListWearViewTest {
+    private ConfigOptionsListWearView configOptionsListWearView;
 
-    @Rule public TestContext testContext = new TestContext();
+//    @Rule public TestContext testContext = new TestContext();
 
+    @Rule public UiThreadTestRule uiThreadTest = new UiThreadTestRule();
+
+    @Rule public ActivityTestRule<TestActivity> activityTestRule = new ActivityTestRule<>(TestActivity.class);
 
     @Test
-    public void showsAListOfConfigOptions() {
+    public void showsOneConfigOption() throws Throwable {
 
-        assertThat("testContext in unit test", testContext.testContext,is(notNullValue()));
-        ConfigOptionsListWearView configOptionsListWearView = new ConfigOptionsListWearView(testContext.testContext);
-        configOptionsListWearView.measure(400,400);
-        configOptionsListWearView.layout(0,0,400,400);
+        final Activity activity = activityTestRule.getActivity();
 
-        configOptionsListWearView.showConfigItems(Collections.singletonList("ANY"));
+        assertNotNull("activity started",activity);
+
+        createUI(activity);
+
+        SystemClock.sleep(TimeUnit.SECONDS.toMillis(1));
 
         ArrayList<View> outViews = new ArrayList<>();
         configOptionsListWearView.findViewsWithText(outViews, "ANY", ViewGroup.FIND_VIEWS_WITH_TEXT);
-
         int numberOfViewsWithText = outViews.size();
-        assertThat(numberOfViewsWithText, is(1));
+
+        assertThat("views in layout with text ANY", numberOfViewsWithText, is(1));
+    }
+
+    private void createUI(final Activity activity) throws Throwable {
+        uiThreadTest.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                configOptionsListWearView = new ConfigOptionsListWearView(activity);
+                configOptionsListWearView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+                activity.setContentView(configOptionsListWearView);
+                configOptionsListWearView.showConfigItems(Collections.singletonList("ANY"));
+            }
+        });
     }
 
 
     public static class ConfigOptionsListWearView extends FrameLayout implements ConfigListView {
 
+        private WearableListView wearableListView;
+
+        private void _ConfigOptionsListWearView() {
+            LayoutInflater.from(getContext()).inflate(R.layout.config_options_list_wear_view,this);
+            wearableListView = (WearableListView) findViewById(R.id.wearable_list);
+        }
+
+
         public ConfigOptionsListWearView(Context context) {
             super(context);
+            _ConfigOptionsListWearView();
         }
 
         public ConfigOptionsListWearView(Context context, AttributeSet attrs) {
             super(context, attrs);
+            _ConfigOptionsListWearView();
         }
 
         public ConfigOptionsListWearView(Context context, AttributeSet attrs, int defStyleAttr) {
             super(context, attrs, defStyleAttr);
+            _ConfigOptionsListWearView();
         }
 
         @TargetApi(Build.VERSION_CODES.LOLLIPOP)
         public ConfigOptionsListWearView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
             super(context, attrs, defStyleAttr, defStyleRes);
+
+            _ConfigOptionsListWearView();
         }
 
         @Override
         public void showConfigItems(List<String> list) {
+            wearableListView.setAdapter(new Adapter(list));
+            wearableListView.invalidate();
+        }
 
-            TextView textView = new TextView(getContext());
-            textView.setText("ANY");
-            addView(textView);
+        public static final class Adapter extends WearableListView.Adapter {
+
+            @Override
+            public WearableListView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                TextView textview = new TextView(parent.getContext());
+                textview.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+                textview.setGravity(Gravity.CENTER);
+                return new WearableListView.ViewHolder(textview);
+            }
+
+            @Override
+            public void onBindViewHolder(WearableListView.ViewHolder holder, int position) {
+                TextView textView = (TextView) holder.itemView;
+                textView.setText(list.get(position));
+            }
+
+            @Override
+            public int getItemCount() {
+                return list.size();
+            }
+
+
+            public Adapter(List<String> list) {
+                this.list = list;
+            }
+
+            private final List<String> list;
+
         }
     }
 
---
+++ wear/src/androidTest/java/uk/co/rossbeazley/wear/android/ui/config/TestActivity.java
@@ -0,0 +1,6 @@
+package uk.co.rossbeazley.wear.android.ui.config;
+
+import android.app.Activity;
+
+public class TestActivity extends Activity {
+}
