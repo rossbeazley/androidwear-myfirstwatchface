77f5253a42d06d4a3fa207b5e3298b44d20ca5c9
rossbeazley <ross@rossbeazley.co.uk>
2016-04-10 14:23:50 +0100

    starting to remove specifics from test

--- core/src/test/java/uk/co/rossbeazley/wear/colour/BackgroundColourConfiguredThroughConfigService.java
+++ core/src/test/java/uk/co/rossbeazley/wear/colour/BackgroundColourConfiguredThroughConfigService.java
@@ -6,7 +6,8 @@ import org.junit.Test;
 import uk.co.rossbeazley.wear.CanBeObserved;
 import uk.co.rossbeazley.wear.Core;
 import uk.co.rossbeazley.wear.android.ui.config.HashMapPersistence;
-import uk.co.rossbeazley.wear.android.ui.config.service.StringPersistence;
+import uk.co.rossbeazley.wear.android.ui.config.TestConfigService;
+import uk.co.rossbeazley.wear.android.ui.config.service.ConfigService;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.nullValue;
@@ -15,48 +16,44 @@ import static org.junit.Assert.assertThat;
 public class BackgroundColourConfiguredThroughConfigService {
 
 
-    private Core core;
-    private CanBeObserved<CanReceiveColourUpdates> colour;
     private Colours.Colour observedBackgroundColour;
-    private CanReceiveColourUpdates canReceiveColourUpdates;
-    private HashMapPersistence persistence;
+    private TestConfigService testConfigService;
+    private ConfigService configService;
 
     @Before
     public void setUp() throws Exception {
-        persistence = new HashMapPersistence();
-        buildCore(persistence);
 
-    }
+        configService = (testConfigService = new TestConfigService()).build();
 
-    private void buildCore(StringPersistence persistence) {
-        core = new Core(persistence);
-        colour = core.canBeObservedForChangesToColour;
-        canReceiveColourUpdates = new CanReceiveColourUpdates() {
+        CanReceiveColourUpdates canReceiveColourUpdates = new CanReceiveColourUpdates() {
             @Override
             public void colourUpdate(Colours to) {
                 observedBackgroundColour = to.background();
             }
         };
-        colour.addListener(canReceiveColourUpdates);
+        testConfigService.core.canBeObservedForChangesToColour.addListener(canReceiveColourUpdates);
         observedBackgroundColour=null;
     }
 
     @Test
     public void notifyOfChange() {
-        core.configService.configureItem("Background");
-        core.configService.chooseOption("Black");
+        configService.configureItem("Background");
+        configService.chooseOption("Black");
         assertThat(observedBackgroundColour,is(Colours.Colour.BLACK));
     }
 
     @Test
     public void notifyOfChangeOnlyWhenConfiguringColourNotAfter() {
 
-        core.configService.configureItem("Background");
-        core.configService.chooseOption("Black");
+        configService.configureItem("Background");
+        configService.chooseOption("Black");
         observedBackgroundColour=null;
 
-        core.configService.configureItem("Rotation");
-        core.configService.chooseOption("South");
+        String differentItem = testConfigService.aDifferentItem("Background");
+        configService.configureItem(differentItem);
+
+        String differentOption = testConfigService.anyOptionForItem(differentItem);
+        configService.chooseOption(differentOption);
 
         assertThat(observedBackgroundColour,is(nullValue()));
     }
