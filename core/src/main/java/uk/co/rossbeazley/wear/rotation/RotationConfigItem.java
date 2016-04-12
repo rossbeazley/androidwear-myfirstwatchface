f3206c48ecab009b0e7b42418529f6a469d67622
rossbeazley <ross@rossbeazley.co.uk>
2016-04-12 22:37:33 +0100

    removed more strings within the rotation config item

--- core/src/main/java/uk/co/rossbeazley/wear/rotation/RotationConfigItem.java
+++ core/src/main/java/uk/co/rossbeazley/wear/rotation/RotationConfigItem.java
@@ -1,5 +1,9 @@
 package uk.co.rossbeazley.wear.rotation;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 import uk.co.rossbeazley.wear.android.ui.config.service.ConfigItem;
 
 public class RotationConfigItem extends ConfigItem {
@@ -8,39 +12,35 @@ public class RotationConfigItem extends ConfigItem {
         this("Rotation");
     }
 
+    Map<String, Orientation> pairs;
 
     public RotationConfigItem(String id) {
         super(id);
-        addOptions("North", "East", "South", "West");
+
+        pairs = new HashMap<>();
+        pairs.put("North", Orientation.north());
+        pairs.put("South", Orientation.south());
+        pairs.put("West", Orientation.west());
+        pairs.put("East", Orientation.east());
+
+        addOptions(pairs.keySet().toArray(new String[pairs.size()]));
         defaultOption("North");
     }
 
     public String optionFor(Orientation orientation) {
-        if (orientation == Orientation.north()) {
-            return  "North";
-        } else if (orientation == Orientation.south()) {
-            return "South";
-        } else if (orientation == Orientation.east()) {
-            return "East";
-        } else if (orientation == Orientation.west()) {
-            return "West";
-        } else {
-            return "";
+
+        Set<Map.Entry<String, Orientation>> entries = pairs.entrySet();
+        String result = null;
+        for (Map.Entry<String, Orientation> el : entries) {
+            if (el.getValue() == orientation) {
+                result = el.getKey();
+                return result;
+            }
         }
+        return result;
     }
 
     public Orientation orientationFor(String rotation) {
-        Orientation result = null;
-        if(rotation.equals("North")) {
-            result = Orientation.north();
-        } else if(rotation.equals("East")) {
-            result = Orientation.east();
-        } else if(rotation.equals("South")) {
-            result = Orientation.south();
-        } else if(rotation.equals("West")) {
-            result = Orientation.west();
-        }
-
-        return result;
+        return pairs.get(rotation);
     }
 }
