ed73685be80dda382775759b72dd59bfacf4a627
rossbeazley <ross@rossbeazley.co.uk>
2016-05-04 13:03:42 +0100

    starts to tease apart the confirmation fragment into some kinda presenter style thingy

--- wear/src/main/java/uk/co/rossbeazley/wear/android/ui/config/ConfigOptionSelectedFragment.java
+++ wear/src/main/java/uk/co/rossbeazley/wear/android/ui/config/ConfigOptionSelectedFragment.java
@@ -5,34 +5,50 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.activity.ConfirmationActivity;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import uk.co.rossbeazley.wear.ui.config.UIEvents;
 
 public class ConfigOptionSelectedFragment extends Fragment implements RaisesUIEvents {
     private UIEvents uiEvents;
 
+    private Map<Integer, Runnable> activityResults = new HashMap<>();
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        showConfirmationTick(new Runnable(){
+            @Override
+            public void run() {
+                System.out.println("TICKFINISHED TICKFINISHED TICKFINISHED TICKFINISHED TICKFINISHED ");
+                uiEvents.optionSelectedFinished();
+            }
+        });
+    }
+
+    private void showConfirmationTick(Runnable resultCallback) {
         Intent intent = new Intent(getActivity(), ConfirmationActivity.class);
         intent.putExtra(ConfirmationActivity.EXTRA_ANIMATION_TYPE,
                 ConfirmationActivity.SUCCESS_ANIMATION);
         intent.putExtra(ConfirmationActivity.EXTRA_MESSAGE,
                 "Saved");
-        startActivityForResult(intent,666);
+        final int requestCode = generateRequestCode();
+        activityResults.put(requestCode,resultCallback);
+        startActivityForResult(intent, requestCode);
+    }
 
+    private int generateRequestCode() {
+        return 666;
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode==666) {
-            System.out.println("TICKFINISHED TICKFINISHED TICKFINISHED TICKFINISHED TICKFINISHED ");
-        }
-        uiEvents.optionSelectedFinished();
+        activityResults.get(requestCode).run();
     }
 
     @Override
     public void injectUIEventsDispatcher(UIEvents uiEvents) {
-
         this.uiEvents = uiEvents;
     }
 }
