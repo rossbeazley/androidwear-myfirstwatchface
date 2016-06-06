6e88a31b88d49c5e079656bd0a07f33091ce40d4
rossbeazley <ross@rossbeazley.co.uk>
2016-06-06 17:43:23 +0100

    Add use of constraint layout, fun!

--- mobile/src/main/java/uk/co/rossbeazley/wear/android/ui/ChosenOptionMobileUIFactory.java
+++ mobile/src/main/java/uk/co/rossbeazley/wear/android/ui/ChosenOptionMobileUIFactory.java
@@ -1,27 +1,28 @@
 package uk.co.rossbeazley.wear.android.ui;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import uk.co.rossbeazley.wear.ChosenOptionView;
+import uk.co.rossbeazley.wear.R;
 import uk.co.rossbeazley.wear.android.ui.config.ConfigItemsPresenter;
 import uk.co.rossbeazley.wear.android.ui.config.SelectableItemListView;
 import uk.co.rossbeazley.wear.android.ui.config.UIFactory;
 import uk.co.rossbeazley.wear.config.ConfigService;
 
-public enum ChosenOptionMobileUIFactory implements UIFactory<TextView> {
+import static android.view.LayoutInflater.from;
+
+public enum ChosenOptionMobileUIFactory implements UIFactory<View> {
     FACTORY {
         @Override
         public View createView(ViewGroup container) {
-            TextView view = new TextView(container.getContext());
-            view.setText("SAVED!");
-            view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-            return view;
+            return from(container.getContext()).inflate(R.layout.success,container,false);
         }
 
         @Override
-        public void createPresenters(ConfigService configService, TextView view) {
+        public void createPresenters(ConfigService configService, View view) {
 
         }
     };
--- mobile/src/main/java/uk/co/rossbeazley/wear/android/ui/SelectAnItemMobileUIFactory.java
+++ mobile/src/main/java/uk/co/rossbeazley/wear/android/ui/SelectAnItemMobileUIFactory.java
@@ -4,24 +4,21 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import uk.co.rossbeazley.wear.SelectAnItemView;
-import uk.co.rossbeazley.wear.android.ui.config.ConfigOptionsPresenter;
-import uk.co.rossbeazley.wear.android.ui.config.SelectableItemListView;
 import uk.co.rossbeazley.wear.android.ui.config.UIFactory;
 import uk.co.rossbeazley.wear.config.ConfigService;
 
-public enum SelectAnItemMobileUIFactory implements UIFactory<TextView> {
+import static android.view.LayoutInflater.from;
+import  uk.co.rossbeazley.wear.R;
+
+public enum SelectAnItemMobileUIFactory implements UIFactory<View> {
     FACTORY {
         @Override
         public View createView(ViewGroup container) {
-            TextView view = new TextView(container.getContext());
-            view.setText("SELECT AN ITEM");
-            view.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-            return view;
+            return from(container.getContext()).inflate(R.layout.choose_item,container,false);
         }
 
         @Override
-        public void createPresenters(ConfigService configService, TextView view) {
+        public void createPresenters(ConfigService configService, View view) {
 
         }
     };
